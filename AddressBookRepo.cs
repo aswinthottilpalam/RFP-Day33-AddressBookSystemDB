20a7a073ea4510cf1197608525a1ebc03b7298cd
aswinthottilpalam <aswinp264@gmail.com>
2022-04-07 11:43:43 +0530

    Create address book table

--- AddressBookRepo.cs
+++ AddressBookRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,62 @@ namespace ADO.NetAddressBookSystem
     {
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book_System;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         SqlConnection connection = new SqlConnection(connectionString);
+
+        public void CheckConnection()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    connection.Open();
+                    Console.WriteLine("Database Connected Successfully....");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Database not Connected!!!");
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public bool AddContact(AddressBookModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("addressProcedure", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@First_Name", model.First_Name);
+                    command.Parameters.AddWithValue("@Last_Name", model.Last_Name);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@Zip", model.Zip);
+                    command.Parameters.AddWithValue("@Phone_Number", model.Phone_Number);
+                    command.Parameters.AddWithValue("@Email", model.Email);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,40 @@ namespace ADO.NetAddressBookSystem
             Console.WriteLine("Welcome To Address Book System (ADO.Net)!");
             Console.WriteLine("AddressBook_Using_Ado.Net ");
             AddressBookRepo abrepo = new AddressBookRepo();
+            AddressBookModel abmodel = new AddressBookModel();
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter Choice \n1.AddContact");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            abmodel.First_Name = "Aswin";
+                            abmodel.Last_Name = "Thottilpalam";
+                            abmodel.Address = "Kozhikode";
+                            abmodel.City = "Calicut";
+                            abmodel.State = "Kerala";
+                            abmodel.Zip = "665544";
+                            abmodel.Phone_Number = "9988774455";
+                            abmodel.Email = "abc@gmail.com";
+                            bool result = abrepo.AddContact(abmodel);
+                            if (result)
+                                Console.WriteLine("Record added successfully...");
+                            else
+                                Console.WriteLine("Some problem is there...");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("please enter integer options only");
+                }
+            }
 
         }
     }
